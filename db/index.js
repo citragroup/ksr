012be87c357dd8d479a5aae15cb7f729a5139bfd
citragroup <122948549+citragroup@users.noreply.github.com>
2024-07-15 22:24:46 +0700

    Update index.js cloud

--- db/index.js
+++ db/index.js
@@ -14,14 +14,8 @@ keyss = [
 ]
 
 
-// key.push(encryptedString5)
-console.log(key)
-//console.log(encryptedString); // 2a3260f5ac4754b8ee3021ad413ddbc11f04138d01fe0c5889a0dd7b4a97e342a4f43bb43f3c83033626a76f7ace2479705ec7579e4c151f2e2196455be09b29bfc9055f82cdc92a1fe735825af1f75cfb9c94ad765c06a8abe9668fca5c42d45a7ec233f0
-//console.log(decryptedString); // bacon
 
 qi = 2
-console.log(cryptr.decrypt(keyss[qi]))
-
 const pool3 = mysql.createPool({
     connectionLimit: 10,
     user: "root",
